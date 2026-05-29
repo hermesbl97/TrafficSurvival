bc8c7d2d46c0cea03d32f17a5ab7f0e82458bd6e
Hermes <a28704@svalero.com>
2026-05-29 14:00:02 +0200

    Implementadas colisiones incluso se desplaza el personaje al chocar

--- core/src/main/java/com/svalero/trafficsurvive/GameScreen.java
+++ core/src/main/java/com/svalero/trafficsurvive/GameScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen implements Screen {
@@ -13,14 +14,19 @@ public class GameScreen implements Screen {
     private SpriteBatch batch;
     private Texture playerTexture;
     private Vector2 playerPosition;
+    private Rectangle playerRectangle;
     private Texture treeTexture;
+    private Rectangle treeRectangle;
 
     @Override
     public void show() {
         playerTexture = new Texture(Gdx.files.internal("tile_0024.png"));
-        treeTexture = new Texture(Gdx.files.internal("tile_0238.png"));
-
         playerPosition = new Vector2(100, 100);
+        playerRectangle = new Rectangle(playerPosition.x, playerPosition.y, playerTexture.getWidth(), playerTexture.getHeight());
+
+        treeTexture = new Texture(Gdx.files.internal("tile_0238.png"));
+        treeRectangle = new Rectangle(350, 200, treeTexture.getWidth(), treeTexture.getHeight());
+
         batch = new SpriteBatch();
     }
 
@@ -29,27 +35,47 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1); // Limpiamos la pantalla
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        batch.draw(playerTexture, playerPosition.x,playerPosition.y);
-        batch.draw(treeTexture, 350, 200);
-
-        batch.end();
+        float oldX = playerPosition.x;
+        float oldY = playerPosition.y;
 
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             playerPosition.x -= 10;
+            playerRectangle.setPosition(playerPosition.x, playerPosition.y);
         }
 
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             playerPosition.x += 10;
+            playerRectangle.setPosition(playerPosition.x, playerPosition.y);
         }
 
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
             playerPosition.y += 10;
+            playerRectangle.setPosition(playerPosition.x, playerPosition.y);
         }
 
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
             playerPosition.y -= 10;
+            playerRectangle.setPosition(playerPosition.x, playerPosition.y);
         }
+
+        // Actualizamos posición después del movimiento
+        playerRectangle.setPosition(playerPosition.x, playerPosition.y);
+
+        //si chocan, lo devolvemos a la posición segura
+        if (playerRectangle.overlaps(treeRectangle)) {
+            playerPosition.x = oldX;
+            playerPosition.y = oldY;
+
+            playerPosition.y -= 5;
+
+            playerRectangle.setPosition(playerPosition.x, playerPosition.y);
+        }
+
+        batch.begin();
+        batch.draw(playerTexture, playerPosition.x,playerPosition.y);
+        batch.draw(treeTexture, treeRectangle.x, treeRectangle.y);
+
+        batch.end();
     }
 
     @Override
